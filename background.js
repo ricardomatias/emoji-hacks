826f277ae8934401e3c97de4752d4f115d446bf5
Ramin <ramin.soleymani@ixds.com>
2015-08-17 19:12:24 +0200

    added 2nd button. still no communication between popup.js and content.js

--- background.js
+++ background.js
@@ -7,3 +7,12 @@
 //     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
 //   });
 // });
+
+/*chrome.browserAction.onClicked.addListener(function(tab) {
+  // Send a message to the active tab
+  console.log("bg action");
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+  });
+});*/
--- popup.js
+++ popup.js
@@ -1,17 +1,19 @@
-'use strict';
-
-function emojiSpan(name, emoji) {
-  return '<span title="' + name.toLowerCase() + '">' + emoji + '</span>';
+function execKeepDOM() {
+ 	//console.log(chrome.tabs);
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	  chrome.tabs.sendMessage(tabs[0].id, {message: "keep"}, function(response) {
+	    console.log("keep");
+	  });
+	});
 }
 
-function execKeepDOM() {
- console.log(chrome.tabs);
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-    console.log("resp");
-  });
-});
-
+function execBrakeDOM() {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	  chrome.tabs.sendMessage(tabs[0].id, {message: "brake"}, function(response) {
+	    console.log("brake");
+	  });
+	});
 }
 
 document.getElementById('keepDOM').addEventListener('click', execKeepDOM);
+document.getElementById('brakeDOM').addEventListener('click', execBrakeDOM);
