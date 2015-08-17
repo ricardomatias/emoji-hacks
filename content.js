e4ba0dd03e963e8b8f850bcca13debf48e68de57
Ramin <ramin.soleymani@ixds.com>
2015-08-17 18:58:02 +0200

    popup.js cant communicate with content.js YET

--- content.js
+++ content.js
@@ -2,6 +2,8 @@
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
+  console.log("some msg");
+  
   if (request.message === "clicked_browser_action") {
 
     console.log("click");
--- popup.js
+++ popup.js
@@ -5,51 +5,13 @@ function emojiSpan(name, emoji) {
 }
 
 function execKeepDOM() {
-	chrome.extension.getBackgroundPage().console.log('log');
-	//chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+ console.log(chrome.tabs);
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+    console.log("resp");
+  });
+});
 
-	
-	
-    var emojisURL = chrome.extension.getURL('emojis.json');
-
-    var emojisJSON = pegasus(emojisURL);
-    var html = document.body.innerHTML;
-
-    chrome.extension.getBackgroundPage().console.log(html);
-	
-    var htmlTextMatches = html.match(/>.*?</gi).filter(function(elem) {
-      return /\w/.test(elem) && elem !== '><';
-    }).join('☃');
-
-    var diff = htmlTextMatches.split('☃');
-
-    emojisJSON.then(function(data) {
-
-    	chrome.extension.getBackgroundPage().console.log('emos in');
-      var name;
-
-      for (var i = 0 ; i < data.length; i++) {
-//      	chrome.extension.getBackgroundPage().console.log(i+"/"+data.length);
-        name = data[i].name;
-
-        name = name.split(' ')[0];
-
-        var patt = new RegExp(name, 'ig');
-
-        htmlTextMatches = htmlTextMatches.replace(patt, emojiSpan(name, data[i].emoji));
-      }
-	 chrome.extension.getBackgroundPage().console.log('loop done');
-      htmlTextMatches.split('☃').forEach(function(elem, idx) {
-        if (elem !== diff[idx]) {
-          html = html.replace(diff[idx], elem);
-        }
-      });
-
-      document.body.innerHTML = html;
-      console.log("done");
-    });
-
-    chrome.extension.getBackgroundPage().console.log('working');
 }
 
 document.getElementById('keepDOM').addEventListener('click', execKeepDOM);
