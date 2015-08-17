a976aafe2f1f9f623b4308558d07f153819d8970
Ricardo Matias <ricardo.matias0@gmail.com>
2015-08-17 16:05:58 +0200

    refactored algorithm to allow styling via .emoji-hacks

--- content.js
+++ content.js
@@ -5,33 +5,53 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
     var emojisURL = chrome.extension.getURL('emojis.json');
 
+    // Get JSON file with emojis
     var emojisJSON = pegasus(emojisURL);
 
     var html = document.body.innerHTML;
 
+    // Grab all the displayed text
     var htmlTextMatches = html.match(/>.*?</gi).filter(function(elem) {
       return /\w/.test(elem) && elem !== '><';
     }).join('☃');
 
+    // Keep the original text to later compare with
     var diff = htmlTextMatches.split('☃');
+
+    var emojis = [];
 
     emojisJSON.then(function(data) {
       var name;
 
-      for (var i = 0 ; i < data.length; i++) {
-        name = data[i].name;
+      data.forEach(function(elem) {
+        name = elem.name;
 
         name = name.split(' ')[0];
 
         var patt = new RegExp(name, 'ig');
 
-        htmlTextMatches = htmlTextMatches.replace(patt, emojiSpan(name, data[i].emoji));
-      }
+        if (patt.test(htmlTextMatches)) {
+          htmlTextMatches = htmlTextMatches.replace(patt, elem.emoji);
 
+          // Keep track of the emojis that will be added
+          emojis.push(elem);
+        }
+      });
+
+      // Compare with the original matches to see which element changed
+      // and replace it with the emoji
       htmlTextMatches.split('☃').forEach(function(elem, idx) {
         if (elem !== diff[idx]) {
           html = html.replace(diff[idx], elem);
         }
+      });
+
+      // replace the emojis with emojis wrapped with tags
+      // this way we will not replace the inserted tags with some emoji
+      emojis.forEach(function(elem) {
+        var patt = new RegExp(elem.emoji, 'ig');
+
+        html = html.replace(patt, emojiSpan(elem.name, elem.emoji));
       });
 
       document.body.innerHTML = html;
@@ -40,5 +60,5 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 });
 
 function emojiSpan(name, emoji) {
-  return '<span title="' + name.toLowerCase() + '">' + emoji + '</span>';
+  return '<span title="' + name.toLowerCase() + '" class="emoji-hacks">' + emoji + '</span>';
 }
